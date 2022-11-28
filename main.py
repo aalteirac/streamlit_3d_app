c37d0edfff59fb0ee63ae948911ea70fc5106a3d
aalteirac <anthony@alteirac.com>
2022-11-28 23:14:54 +0100

    bumpt

--- main.py
+++ main.py
@@ -34,8 +34,11 @@ with st.expander("Code"):
     md=st.selectbox("3D Model:",["https://alteirac.com/models/helmet/scene.gltf",
                                  "https://alteirac.com/models/engine/scene.gltf"
                                  ])
-    value = sd.streamlit_3d(model=md,points=[{"description":"LEFT_LIGHT",
-                                            "data-position":{"x":0.4595949207254826,"y":0.40998085773554555,"z":0.33846317660071373},
-                                            "data-normal":{"x":-0.18705895743345607,"y":-0.3420641705224677,"z":0.9208697246020658}}
+    # if you want to load existing Annotations:                                
+    value = sd.streamlit_3d(model=md,points=[{{"description":"LEFT_LIGHT",
+                                            "data-position":{{"x":0.4595949207254826,"y":0.40998085773554555,"z":0.33846317660071373}},
+                                            "data-normal":{{"x":-0.18705895743345607,"y":-0.3420641705224677,"z":0.9208697246020658}}}}
                                             ])
+    # if you simply want show the model:
+    # value = sd.streamlit_3d(model=md)                                        
     st.write(value)''' ,language="python")
